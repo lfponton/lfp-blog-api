1a6ab90f887ff6d401dc950fa86c1ab749750e00
Luis <lfponton@gmail.com>
2024-02-06 16:55:59 +0100

    add schema and documentation

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,13 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    
     class Meta: 
+        model = Post
         fields = (
             "id",
             "author",
@@ -12,4 +15,10 @@ class PostSerializer(serializers.ModelSerializer):
             "body",
             "created_at",
         )
-        model = Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta: 
+        model = get_user_model()
+        fields = ("id", "username",)
